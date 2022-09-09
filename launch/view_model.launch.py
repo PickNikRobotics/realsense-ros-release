1010d06f09072c7730a19a7a86ebf4d62f4220df
JafarAbdi <cafer.abdi@gmail.com>
2022-09-09 11:04:50 -0600

    Rebase from 'release/humble/realsense2_description'

--- launch/view_model.launch.py
+++ launch/view_model.launch.py
@@ -16,7 +16,6 @@
 import os
 import launch
 from launch_ros.actions import Node
-import launch.events
 from ament_index_python.packages import get_package_share_directory
 import sys
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
@@ -30,7 +29,7 @@ def generate_launch_description():
         print ('ros2 launch realsense2_description view_model.launch.py model:=<model>')
         print ('Available argumants for <model> are as follows:')
         print ('\n'.join(available_urdf_files))
-        return launch.LaunchDescription()
+        exit(-1)
 
     rviz_config_dir = os.path.join(get_package_share_directory('realsense2_description'), 'rviz', 'urdf.rviz')
     xacro_path = os.path.join(get_package_share_directory('realsense2_description'), 'urdf', params['model'])
